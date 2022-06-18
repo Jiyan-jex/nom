05e2cd31dffd48be4a84d70ad53383d464fac84c
Jejo <jean20ahmed@gmail.com>
2022-06-18 21:57:35 +0300

    update
    update

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use std::error;
+use std::io::{Error, ErrorKind};
+use nom::character::complete::{anychar, char, one_of};
+use nom::combinator::opt;
+use nom::multi::many_till;
+use nom::number::complete::float;
+use nom::sequence::{delimited, terminated};
+use nom::IResult;
+const PARSE_ERROR: &str = "failed to parse";
+#[derive(Debug)]
+pub(crate) struct Todo {
+    pub done: bool,
+    pub task: String,
+    pub time: Option<f32>,
+}
+fn time(s: &str) -> IResult<&str, Option<f32>> {
+    let (s, time) = delimited(char('('), opt(float), char(')'))(s)?;
+    Ok((s, time))   }
+fn done(s: &str) -> IResult<&str, bool> {
+    let (s, done) = delimited(char('['), one_of("x "), char(']'))(s)?;
+    Ok((s, done == 'x'))    }
+fn todo(s: &str) -> IResult<&str, Todo> {
+    let (s, done) = done(s)?;
+    let (s, (task, time)) = many_till(anychar, terminated(time, char('\n')))(s)?;
+    dbg!(&task);
+    let mut task = task;
+    if task[0] != ' ' || task.remove(0) != ' ' || task.pop() != Some(' ') {
+        return Err(nom::Err::Error(nom::error::Error::new(
+            PARSE_ERROR,
+            nom::error::ErrorKind::Char,
+        )));    }
+    let task = task.iter().collect();
+    let todo = Todo { done, task, time };
+    Ok((s, todo))   }
+impl Todo {
+    pub(crate) fn serialize(&self) -> String {
+        format!(
+            "[{}] {} ({})\n",
+            if self.done { 'x' } else { ' ' },
+            self.task,
+            self.time.map_or("".to_string(), |f| format!("{:.1}", f)),  ) }
+    pub(crate) fn deserialize(
+        s: &str,
+    ) -> Result<Todo, Box<dyn error::Error + Send + Sync + 'static>> {
+        match todo(s) {
+            Ok((s, todo)) if s.is_empty() => Ok(todo),
+            _ => Err(Box::new(Error::new(ErrorKind::InvalidInput, PARSE_ERROR))),       } } }
+
+fn main() {
+
+    let d = Todo::deserialize("");
+    dbg!(d);
+}
+
+//fn add(s: &str) -> String {
+//    Todo {
+//        done: false,
+//        task: s.to_string(),
+//        time: None,
+//    }
+//    .deserialize()
+//}
